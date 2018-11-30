92437a8538de7f7a21bceca81cadebcd05c2493c
deutode <desiree.deuto@wisebatt.com>
2018-11-30 18:40:41 +0100

    update schema

--- models/componentModel.js
+++ models/componentModel.js
@@ -3,147 +3,87 @@ const Schema = mongoose.Schema;
 
 
 const componentSchema = new Schema({
-  error: Boolean,
-  data: [
-    {
-      name: String,
-      version: String,
-      description: String,
-      components: [
-        {
-          datasheet: String,
-          tech_specs: [
-            {
-              value: String,
-              key: String
-            },
-            {
-              value: String,
-              key: String
-            },
-            {
-              value: String,
-              key: String
-            }
-          ],
-          detailled_info: String,
-          v_min: Number,
-          io: {
-            power: [
-              {
-                wiretype: String,
-                label: String,
-                type: String
-              }
-            ],
-            gates: [
-              {
-                type: String,
-                type: String,
-                type: String,
-                type: String,
-                wiretype: String,
-                sim_gate: [Number],
-                label: String,
-                type: String
-              },
-              {
-                wiretype: String,
-                sim_gate: [Number],
-                label: String,
-                type: String
-              },
-              {
-                wiretype: String,
-                sim_gate: [Number],
-                label: String,
-                type: String
-              },
-              {
-                wiretype: String,
-                sim_gate: [Number],
-                label: String,
-                type: String
-              },
-              {
-                wiretype: String,
-                sim_gate: [Number],
-                label: String,
-                type: String
-              },
-              {
-                wiretype: String,
-                sim_gate: [Number],
-                label: String,
-                type: String
-              }
-            ]
-          },
-          configuration_parameters: [
-            {
-              default_value: Number,
-              information: [],
-              description: String,
-              range: [Number],
-              type: String,
-              name: String
-            },
-            {
-              default_value: String,
-              information: [],
-              description: String,
-              range: [String],
-              type: Number,
-              name: String
-            }
-          ],
-          label: String,
-          name: String,
-          v_max: Number,
-          categorie: String,
-          i_min: String,
-          icon_text: String,
-          power: String,
-          states: String,
-          i_max: Number,
-          version_launch: String,
-          uuid: String,
-          details: String,
-          power_specs: [
-            {
-              data: [
-                {
-                  value: Number,
-                  key: String
-                },
-                {
-                  value: Number,
-                  key: String
-                }
-              ],
-              type: String
-            },
-            {
-              data: [
-                {
-                  value: Number,
-                  key: String
-                },
-                {
-                  value: Number,
-                  key: String
-                }
-              ],
-              type: String
-            }
-          ],
-          icon: String,
-          type: String,
-          vendor: String
-        }      
-      ]
-    }
+  datasheet: {
+		type: 'String'
+	},
+	tech_specs: {
+		type: [
+			'Mixed'
+		]
+	},
+	detailled_info: {
+		type: 'String'
+	},
+	v_min: {
+		type: 'Number'
+	},
+	io: {
+		power: {
+			type: [
+				'Mixed'
+			]
+		},
+		gates: {
+			type: [
+				'Mixed'
+			]
+		}
+	},
+	label: {
+		type: 'String'
+	},
+	name: {
+		type: 'String'
+	},
+	v_max: {
+		type: 'Number'
+	},
+	categorie: {
+		type: 'String'
+	},
+	i_min: {
+		type: 'Number'
+	},
+	icon_text: {
+		type: 'String'
+	},
+	power: {
+		type: 'String'
+	},
+	states: {
+		type: 'String'
+	},
+	i_max: {
+		type: 'Number'
+	},
+	version_launch: {
+		type: 'String'
+	},
+	uuid: {
+		type: 'String'
+	},
+	details: {
+		type: 'String'
+	},
+	power_specs: {
+		type: [
+			'Mixed'
+		]
+	},
+	icon: {
+		type: 'String'
+	},
+	type: {
+		type: 'String'
+	},
+	vendor: {
+		type: 'String'
+	},
+configuration_parameters: {
+  type: [
+    'Mixed'
   ]
+}
 });
 
 module.exports = mongoose.model('Component', componentSchema);
